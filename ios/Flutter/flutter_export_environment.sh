707fd08c024cecb065e10df07dfc095e208416b8
Ade Rizky <ade@MacBook-Pro.local>
2022-03-26 16:04:50 +0700

    first commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/ade/MobilePrograming/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ade/MobilePrograming/Project/Kuliah/uts_191011401763/uts_191011401763"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
